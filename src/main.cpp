89a97ae84fd9b29cda083f3a681ebe073e179827
santoserick0031@gmail.com <santoserick0031@gmail.com>
2025-09-18 19:53:42 -0300

    alteração

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,34 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>//criar serivdor wev
+#include <SPIFFS.h>//pegar o html
 #include "routs/routs.h"
-#include <Arduino.h>      // Biblioteca principal do Arduino, inclui funções básicas
-#include <WiFi.h>        
-//cria um acces point (cria uma rede a outros dispositivos podem se conectar)
-#include <WebServer.h>    // Biblioteca para criar um servidor web simples
-//o esp fica responde as requisições http
-#include <SPIFFS.h>       // Biblioteca para acessar o sistema de arquivos SPIFFS
 
-// Define o nome e a senha da rede WiFi que o ESP32 vai criar (modo Access Point)
-#define AP_SSID "ESP32_REDE"       // Nome da rede WiFi (SSID)
-#define AP_SENHA "12345678"        // Senha da rede WiFi
+#define AP_SSID  "ESP32_REDE"
+#define AP_SENHA "12345678"
 
-WebServer server(80); 
-//roda o servidor na porta 80 (porta padrão para HTTP)
+WebServer server(80);  
+// cria um abjeto que representa o servidor
 
 void setup() {
-  Serial.begin(115200); // escolhe a velocidade da comunicação serial
-  pinMode(LED_BUILTIN, OUTPUT);   // Define o pino 2 (GPIO2) como saída (para o LED)
-
-  // serve pra acessar arquivos do pc que foi refenciado no código
+  Serial.begin(115200);  
   if (!SPIFFS.begin(true)) {
-    Serial.println("Erro ao montar SPIFFS"); // Mostra erro se não conseguir montar
-    return; // Sai do setup se falhar
+    Serial.println("Erro ao montar SPIFFS");
+    return;
   }
 
-  // Cria uma rede WiFi própria (Access Point)
-  if (WiFi.softAP(AP_SSID, AP_SENHA)==true){
-  Serial.println("Rede criada!");
-  Serial.print("IP do ESP32: ");
-  Serial.println(WiFi.softAPIP());  // Mostra o IP do ESP32 (normalmente 192.168.4.1)
+  if (WiFi.softAP(AP_SSID, AP_SENHA)) {
+    Serial.println("Rede criada!");
+    Serial.print("IP do ESP32: ");
+    Serial.println(WiFi.softAPIP());// o serial.printa o ip do esp32
   } else {
     Serial.println("Erro ao criar a rede");
-    return; // Sai do setup se falhar
+    return;
   }
- // Chama rotas do servidor web
-  configurarRotas();
+
+  configurarRotas();  // Registra rotas e inicia o servidor
 }
 
 void loop() {
-  server.handleClient(); // Mantém o servidor web rodando e responde às requisições
+  server.handleClient();
 }
